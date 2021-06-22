3ffd4b80aeeaf62902a515e1f7f7471c5e203f78
gjeffgolden <gjgolden08@gmail.com>
2021-06-22 10:54:13 -0600

    Added CustomMap class.

---
+++ src/CustomMap.ts
@@ -0,0 +1,15 @@
+//by creating this class, allows us to reduce functionality from main Google Maps object
+
+export class CustomMap {
+    private googleMap: google.maps.Map
+
+    constructor(divId: string) {
+        this.googleMap = new google.maps.Map(document.getElementById(divId), {
+            zoom: 1,
+            center: {
+                lat: 0,
+                lng: 0
+            }
+        }) 
+    }
+}
