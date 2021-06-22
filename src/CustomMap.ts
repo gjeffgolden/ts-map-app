605a4256d9e4b3aaa1023cda9570b2be5a910a95
gjeffgolden <gjgolden08@gmail.com>
2021-06-22 11:17:40 -0600

    Added map markers.

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,8 @@
 //by creating this class, allows us to reduce functionality from main Google Maps object
 
+import { User } from './User'
+import { Company } from './Company'
+
 export class CustomMap {
     private googleMap: google.maps.Map
 
@@ -12,4 +15,25 @@ export class CustomMap {
             }
         }) 
     }
+
+    addUserMarker(user: User): void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: user.location.lat,
+                lng: user.location.lng
+            }
+        })
+    }
+
+    addCompanyMarker(company: Company): void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: company.location.lat,
+                lng: company.location.lng
+            }
+        })
+    }
+
 }
